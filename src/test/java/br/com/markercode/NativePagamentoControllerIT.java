9eedb60d1bb800fbcd9c6650ed50f3ec8fb2caf6
Gustavo Faria <gustavofaria@Mac-mini-de-Gustavo.local>
2020-06-08 15:21:25 -0300

    semTeste2

--- src/test/java/br/com/markercode/NativePagamentoControllerIT.java
+++
@@ -1,9 +0,0 @@
-package br.com.markercode;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativePagamentoControllerIT extends PagamentoControllerTest {
-
-    // Execute the same tests but in native mode.
-}
